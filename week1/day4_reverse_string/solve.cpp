0d0bfd88d51ec7661e5d25e9bcbafbf87c4fe00e
Arul Agrawal <me@arul.io>
2020-06-04 09:56:29 +0200

    week1 day4

---
+++ week1/day4_reverse_string/solve.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    void reverseString(vector<char> &s) {
+        int p1 = 0, p2 = s.size() - 1;
+        while (p1 < p2) {
+            auto temp = s[p1];
+            s[p1] = s[p2];
+            s[p2] = temp;
+            p1++, p2--;
+        }
+    }
+};
